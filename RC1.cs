ccead5f9a29af644246e65677358609972415950
Billy Kozma <wkozma@ntia.doc.gov>
2025-06-02 00:57:12 -0600

    adding some comments

--- RC1.cs
+++ RC1.cs
@@ -25,74 +25,87 @@ namespace ClutterAnalysis
             HighRise = 2
         };
 
-        private static double[] a_k = new[] { 4.9, -2.6, 2.4 };
-        private static double[] b_k = new[] { 6.7, 6.6, 7 };
-        private static double[] c_k = new[] { 2.6, 2, 1 };
+        static readonly double[] a_k = new[] { 4.9, -2.6, 2.4 };
+        static readonly double[] b_k = new[] { 6.7, 6.6, 7 };
+        static readonly double[] c_k = new[] { 2.6, 2, 1 };
 
-        private static double[] a_c = new[] { 0.19, 0.42, 0.19 };
-        private static double[] b_c = new[] { 0, -6.7, -2.7 };
+        static readonly double[] a_c = new[] { 0.19, 0.42, 0.19 };
+        static readonly double[] b_c = new[] { 0, -6.7, -2.7 };
 
-        private static double[] a_v = new[] { 1.4, 0.15, 0.15 };
-        private static double[] b_v = new[] { 74.0, 97, 98 };
+        static readonly double[] a_v = new[] { 1.4, 0.15, 0.15 };
+        static readonly double[] b_v = new[] { 74.0, 97, 98 };
 
-        private static double[] a_kprime = new[] { 6, 3.6, 5 };
-        private static double[] b_kprime = new[] { 0.07, 0.05, 0.003 };
+        static readonly double[] a_kprime = new[] { 6, 3.6, 5 };
+        static readonly double[] b_kprime = new[] { 0.07, 0.05, 0.003 };
 
-        private static double[] a_cprime = new[] { 0.15, 0.17, 0.17 };
-        private static double[] b_c1prime = new[] { 5.4, 13, 32.6 };
-        private static double[] b_c2prime = new[] { -0.3, -0.2, 0.012 };
-        private static double[] b_c3prime = new[] { 3.2, 3.7, -23.9 };
-        private static double[] b_c4prime = new[] { 0.07, 0.05, -0.07 };
-        private static double[] c_cprime = new double[] { -27, -41, -41 };
+        static readonly double[] a_cprime = new[] { 0.15, 0.17, 0.17 };
+        static readonly double[] b_c1prime = new[] { 5.4, 13, 32.6 };
+        static readonly double[] b_c2prime = new[] { -0.3, -0.2, 0.012 };
+        static readonly double[] b_c3prime = new[] { 3.2, 3.7, -23.9 };
+        static readonly double[] b_c4prime = new[] { 0.07, 0.05, -0.07 };
+        static readonly double[] c_cprime = new double[] { -27, -41, -41 };
 
-        private static double[] a_vprime = new[] { 1.6, 1, 1 };
-        private static double[] b_vprime = new double[] { -17, -21, -18 };
+        static readonly double[] a_vprime = new[] { 1.6, 1, 1 };
+        static readonly double[] b_vprime = new double[] { -17, -21, -18 };
 
-        private static double[] a_N = new[] { 8.54, 0.056 };
-        private static double[] b_N = new[] { 17.57, 6.32 };
-        private static double[] c_N = new[] { 0.63, 0.19 };
+        static readonly double[] a_N = new[] { 8.54, 0.056 };
+        static readonly double[] b_N = new[] { 17.57, 6.32 };
+        static readonly double[] c_N = new[] { 0.63, 0.19 };
 
         static public double Invoke(double f__ghz, double theta__deg,
             double p, double h__meter, Environment env)
         {
             // compute probability of LOS
+            //////////////////////////////////
+            
+            // equation 8(a-c)
             double V_max = Math.Min(a_v[(int)env] * h__meter + b_v[(int)env], 100);
             double C_e = a_c[(int)env] * h__meter + b_c[(int)env];
             double k = Math.Pow((h__meter + a_k[(int)env]) / b_k[(int)env], c_k[(int)env]);
+            // equation 7
             double p_LOS = V_max * ((1 - Math.Exp(-k * (theta__deg + C_e) / 90)) / (1 - Math.Exp(-k * (90 + C_e) / 90)));
 
             // compute conditional probability of FcLOS | LOS
+            ///////////////////////////////////////////////////
+            
+            // equation 10(a-c)
             double kprime = a_kprime[(int)env] * Math.Exp(b_kprime[(int)env] * h__meter);
             double C_eprime = Math.Pow(f__ghz * 1e9, a_cprime[(int)env]) + 
                               b_c1prime[(int)env] * Math.Exp(b_c2prime[(int)env] * h__meter) + 
                               b_c3prime[(int)env] * Math.Exp(b_c4prime[(int)env] * h__meter) +
                               c_cprime[(int)env];
             double V_maxprime = Math.Min(a_vprime[(int)env] * h__meter + b_vprime[(int)env], 0) * Math.Pow(f__ghz, -0.55) + 100;
+            // equation 9
             double p_FcLOS_LOS = V_maxprime * ((1 - Math.Exp(-kprime * (theta__deg + C_eprime) / 90)) / (1 - Math.Exp(-kprime * (90 + C_eprime) / 90)));
 
             // probability of being Fresnel clear LOS
+            // equation 11
             double p_FcLOS = p_LOS * p_FcLOS_LOS / 100;
 
             // NLOS clutter loss
+            // equation 13(a-b)
             double mu = a_N[0] + b_N[0] * Math.Log(1 + (90 - theta__deg) / 90) + Math.Pow(f__ghz, c_N[0]);
             double sigma = a_N[1] + b_N[1] * Math.Log(1 + (90 - theta__deg) / 90) + Math.Pow(f__ghz, c_N[1]);
             NormalDistribution norm = new NormalDistribution(mu, sigma);
+            // equation 12
             //double p_temp = (100 * p * (1 - p_LOS)) / (100 - p_LOS);
-            double L_clt_NLOS = Math.Max(norm.InverseDistributionFunction(p / 100), 6);
+            double L_clt_NLOS__db = Math.Max(norm.InverseDistributionFunction(p / 100), 6);
 
             double L_clt__db;
             if (0 <= p && p <= p_FcLOS)
             {
+                // equation 15
                 L_clt__db = Math.Log10(p / p_FcLOS) / Math.Log10(30);
             }
             else if (p <= p_LOS)
             {
+                // equation 14
                 L_clt__db = 6 / (p_LOS - p_FcLOS) * (p - p_FcLOS);
             }
             else
             {
                 // NLOS
-                L_clt__db = L_clt_NLOS;
+                L_clt__db = L_clt_NLOS__db;
             }
 
             return L_clt__db;
